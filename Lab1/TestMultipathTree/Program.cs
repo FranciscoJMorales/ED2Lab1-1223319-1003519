6da20c5e34ac22a6caa01b17f712e33f1b1a8a44
Estuardoxd <Estuardovilleda2001@gmail.com>
2020-09-01 21:38:06 -0600

    Se realizo la segunda serie, sujeto a cambios (inserción arbol implementada)

--- Lab1/TestMultipathTree/Program.cs
+++ Lab1/TestMultipathTree/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-
+using Generics;
+using TestMultipathTree;
 namespace TestMultipathTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int grado, insertar = 1;
+            try
+            {
+                Console.WriteLine("Ingrese el numero del grado para el arbol");
+                grado = int.Parse(Console.ReadLine());
+                MultipathTree<int> tree = new MultipathTree<int>(grado);
+
+                do
+                {
+                    insertarArbol(tree);
+                    Console.WriteLine("¿Desea seguir ingresando valores? 1.Si 2.No");
+                    insertar = int.Parse(Console.ReadLine());
+                } while (insertar == 1);
+
+            }
+            catch
+            {
+                Console.WriteLine("Error producido no controlado");
+            }
+           
+            
+        }
+
+        static void insertarArbol(MultipathTree<int> tree)
+        {
+            int valor;
+            Console.WriteLine("Ingrese el valor a ingresar");
+            tree.Add(valor = int.Parse(Console.ReadLine()));
+           
+        
         }
     }
 }
